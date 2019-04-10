7344972bfa05f33b17427c748bd1983836c1d1b9
johnhaus <john.haus@outlook.com>
2019-04-10 15:17:58 -0300

    few fixes to get app working

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import reducers from './reducers';
 
 // internal modules
